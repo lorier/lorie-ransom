f21ee81bf7443a156e52d7daa0ebf76bbc503d00
LORIE RANSOM <lrmacbook@LORIEs-MacBook-Pro.local>
2016-04-30 09:37:18 -0700

    add chart.js, bower

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,7 +11,27 @@
   'use strict';
 
   $(function () {
-    // FastShell
-  });
+  	var ctx = document.getElementById('myChart');
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [12, 19, 3, 5, 2, 3]
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+	});
+
 
 })(jQuery, window, document);
